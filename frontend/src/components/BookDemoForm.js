6f7fec198d2b59141b7a019af817bb0dae6327d7
Rishav01-jpg <kalart496@gmail.com>
2025-10-07 08:57:08 +0530

    changed the link

--- frontend/src/components/BookDemoForm.js
+++ frontend/src/components/BookDemoForm.js
@@ -34,7 +34,7 @@ const BookDemoForm = ({ open, onClose }) => {
     setError('');
     
     try {
-      const response = await axios.post('http://localhost:5000/api/demo/book', formData);
+      const response = await axios.post('https://ring-ring-eq46.onrender.com/api/demo/book', formData);
       
       if (response.data.success) {
         setSuccess(true);
